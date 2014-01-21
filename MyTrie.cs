ccdb89ef999e4cfc2613ff59009e9bb9f5a1e263
benlee429 <blee429@gmail.com>
2014-01-21 15:37:35 +0900

    added entire project folder to git. fresh start

---
+++ MyTrie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pa2
+{
+    public class MyTrie
+    {
+        private MyTrieNode root;
+        public MyTrie()
+        {
+            //process the wiki dump
+            //use currentNode.addKid(newNode);
+            this.root = new MyTrieNode("test");
+        }
+    }
+}
---
+++ MyTrieNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pa2
+{
+    public class MyTrieNode
+    {
+        private string current;
+        private Dictionary<string, MyTrieNode> kids;
+        private string word;
+
+        public MyTrieNode(string current)
+        {
+            this.current = current;
+            this.kids = new Dictionary<string, MyTrieNode>();
+        }
+
+        public void addKid(MyTrieNode newKid)
+        {
+            this.kids.Add(newKid.getString(), newKid);
+        }
+
+        public string getString()
+        {
+            return this.current;
+        }
+        public string setWord(string word){
+            this.word = word;
+            this.kids = null;
+        }
+    }
+}
