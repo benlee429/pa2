0fe7625041a51500537edcaf855f8c2890a563df
benlee429 <blee429@gmail.com>
2014-01-21 11:50:15 +0900

    aspx returns a string

--- search.js
+++ search.js
@@ -6,12 +6,13 @@ $(document).ready(function() {
 			url: "search2.aspx/GetQuery", 
 			contentType: "application/json; charset=utf-8",
 			type: "POST",
-			dataType: "html",
+			dataType: "text",
 			data: JSON.stringify({searchTerm: input}),
 			error: ajaxFailure,
 			success: function(data){
-				alert("success: " + data);//
-				$('#results').append(data);
+				var newTerm = $( document.createElement('div') );
+				newTerm.text(data);
+				$('#results').append(newTerm);
 			}
 		}) 
 	});
