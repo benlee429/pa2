43c96e22eccacc6567db566a8a373d06b1e96113
benlee429 <blee429@gmail.com>
2014-01-21 11:55:53 +0900

    added .d

--- search.js
+++ search.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 			success: function(data){
 				alert(data);
 				var newTerm = $( document.createElement('div') );
-				newTerm.text(data);
+				newTerm.text(data.d);
 				$('#results').append(newTerm);
 			}
 		}) 
